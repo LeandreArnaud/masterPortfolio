8d2b48b93fe28cb2ef9d44ab6650e3a56e19a183
LeandreArnaud <pro.leandre.arnaud@gmail.com>
2021-12-25 20:24:39 +0100

    changes favicon

--- src/portfolio.js
+++ src/portfolio.js
@@ -439,7 +439,7 @@ const publicationsHeader = {
 const contactPageData = {
   contactSection: {
     title: "Contact Me",
-    profile_image_path: "leandre-arnaud.jpg",
+    profile_image_path: "leandre.jpeg",
     description:
       "I am available on almost every social media. You can message me, I will reply within 24 hours. I can help you with ML, AI, React, React Native, Cloud and Opensource Development.",
   },
